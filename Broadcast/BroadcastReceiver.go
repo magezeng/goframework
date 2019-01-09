ff9de24a21dd2efc20bc10b66d1829f7983c0ade
曾祥林 <429801517@qq.com>
2019-01-09 10:47:43 +0800

    增加双向链表

--- Broadcast/BroadcastReceiver.go
+++ Broadcast/BroadcastReceiver.go
@@ -2,9 +2,5 @@ package Broadcast
 
 type BroadcastReceiver struct {
 	id             string
-	reveiceChannel chan BroadcastContent
-}
-
-func (receiver *BroadcastReceiver) ReceiveMessage() (content BroadcastContent) {
-	return <-receiver.reveiceChannel
+	ReveiceChannel chan BroadcastContent
 }
