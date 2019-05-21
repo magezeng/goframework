fe970424c4fa3e1bf67c4144551882f1eaaa1705
wangyal <nuttertools07@hotmail.com>
2019-05-21 19:22:32 +0800

    修改upload方法使之不依赖于gin

--- Utils/GinUpload.go
+++ Utils/GinUpload.go
@@ -2,39 +2,54 @@ package Utils
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"log"
-	"tipu.com/go-framework/Code"
-	"tipu.com/go-framework/Models"
+	"io"
+	"mime/multipart"
+	"os"
 )
 
 // 上传单个文件，保存到path指定的位置上
-func UploadFile(c *gin.Context, path string) {
-	file, _ := c.FormFile("file")
+func UploadFile(file *multipart.FileHeader, path string) error {
 	// Upload the file to specific dst.
-	err := c.SaveUploadedFile(file, path+file.Filename)
+	err := saveToDisk(file, path+file.Filename)
 	if err != nil {
 		e := fmt.Errorf("文件[%s]上传失败，错误信息：%s", file.Filename, err.Error())
-		Models.ResultFail(c, Code.UPLOAD_ERROR, e)
-		return
+		return e
 	}
-	Models.ResultSuccessWithMsg(c, nil, "上传成功！")
+	return nil
 }
 
-// 上传多个文件，保存到path指定的位置上
-// TODO: 根据webuploader传递的List创建文件层级结构
-func UploadFiles(c *gin.Context, path string) {
-	form, _ := c.MultipartForm()
-	files := form.File["upload[]"]
-	for _, file := range files {
-		log.Println(file.Filename)
-		err := c.SaveUploadedFile(file, file.Filename)
-		if err != nil {
-			e := fmt.Errorf("文件[%s]上传失败，错误信息：%s", file.Filename, err.Error())
-			Models.ResultFail(c, Code.UPLOAD_ERROR, e)
-			return
-		}
+//// 上传多个文件，保存到path指定的位置上
+//// TODO: 根据webuploader传递的List创建文件层级结构
+//func UploadFiles(c *gin.Context, path string) {
+//	form, _ := c.MultipartForm()
+//	files := form.File["upload[]"]
+//	for _, file := range files {
+//		log.Println(file.Filename)
+//		err := c.SaveUploadedFile(file, file.Filename)
+//		if err != nil {
+//			e := fmt.Errorf("文件[%s]上传失败，错误信息：%s", file.Filename, err.Error())
+//			Models.ResultFail(c, Code.UPLOAD_ERROR, e)
+//			return
+//		}
+//	}
+//	// TODO: 这个长度不一定正确
+//	Models.ResultSuccessWithMsg(c, nil, fmt.Sprintf("全部文件上传成功，共上传了%d个文件", len(files)))
+//}
+
+// 把上传的文件保存到本地
+func saveToDisk(file *multipart.FileHeader, dst string) error {
+	srcFile, err := file.Open()
+	if err != nil {
+		return err
 	}
-	// TODO: 这个长度不一定正确
-	Models.ResultSuccessWithMsg(c, nil, fmt.Sprintf("全部文件上传成功，共上传了%d个文件", len(files)))
+	defer srcFile.Close()
+
+	outFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, srcFile)
+	return err
 }
