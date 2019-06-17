90d5592a5a9d3d4c9a1229cc9c31ff29fac3d82c
withcancer <nuttertools07@hotmail.com>
2019-06-17 15:07:16 +0800

    修改了result的模型，去除了list类型

--- Models/Result.go
+++ Models/Result.go
@@ -1,25 +1,17 @@
 package Models
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strategy-control-platform/strategy-control-platform-server/Model"
 	"tipu.com/go-framework/Code"
 )
 
 // Result 返回结果时的通用结构
 type Result struct {
-	Code int16       `json:"code"`
+	Code uint16      `json:"code"`
 	Data interface{} `json:"data"`
 	Msg  string      `json:"msg"`
-}
-
-// ResultList 列表型结果的结构
-type ResultList struct {
-	Code  int16         `json:"code"`
-	Rows  []interface{} `json:"rows"`
-	Total int           `json:"total"`
-	Msg   string        `json:"msg"`
 }
 
 // ResultSuccess 返回结果的标准方式，传递结构体数据
@@ -29,20 +21,14 @@ func ResultSuccess(ctx *gin.Context, data interface{}) {
 
 // ResultSuccess 返回结果的标准方式，传递结构体数据和消息
 func ResultSuccessWithMsg(ctx *gin.Context, data interface{}, msg string) {
-	ctx.JSON(http.StatusOK, &Model.Result{Code: Code.NORMAL, Data: data, Msg: msg})
-}
-
-// ResultListSuccess 返回列表结果，需要传泛型数组和消息
-func ResultListSuccess(ctx *gin.Context, data []interface{}, total int) {
-	ResultListSuccessWithMsg(ctx, data, total, "")
-}
-
-// ResultListSuccess 返回列表结果，需要传泛型数组和消息
-func ResultListSuccessWithMsg(ctx *gin.Context, data []interface{}, total int, msg string) {
-	ctx.JSON(http.StatusOK, &Model.ResultList{Code: Code.NORMAL, Rows: data, Total: total, Msg: msg})
+	ctx.JSON(http.StatusOK, Result{Code: Code.NORMAL, Data: data, Msg: msg})
 }
 
 // ResultFail 返回错误结果及错误code
 func ResultFail(ctx *gin.Context, code uint16, err error) {
-	ctx.JSON(http.StatusOK, &Model.Result{Code: code, Msg: err.Error()})
+	ctx.JSON(http.StatusOK, Result{Code: code, Msg: err.Error()})
+}
+
+func (r Result) String() string {
+	return fmt.Sprint("Code: %d, 内容: %s, 数据: %s", r.Code, r.Msg, r.Data)
 }
