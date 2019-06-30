696d2e0e6d47506ec393e2d25a553012830046d2
zengxianglin <429801517@qq.com>
2019-06-30 15:12:44 +0800

    创建MasterNodeModel共有对象中心

--- Models/MasterNodeModel/CoinMasterNode.go
+++ Models/MasterNodeModel/CoinMasterNode.go
@@ -1,4 +1,4 @@
-package Models
+package MasterNodeModel
 
 import (
 	"github.com/jinzhu/gorm"
@@ -8,7 +8,7 @@ import (
 type CoinMasterNode struct {
 	gorm.Model
 	// 用于指示这个节点位于哪个服务器上面
-	CoinName      string    `gorm:"not null;type:varchar(20)" form:"coin_name" binding:"required,max=20" json:"ip"`
+	CoinName      string    `gorm:"not null;type:varchar(20)" form:"coin_name" binding:"required,max=20" json:"coin_name"`
 	StartAt       time.Time `gorm:"type:timestamp" form:"start_at" json:"start_at"`
 	Status        string    `gorm:"type:varchar(200)" form:"status" binding:"required" json:"status"`
 	PayAddress    string    `gorm:"type:varchar(100)" form:"pay_address" binding:"required,max=100" json:"pay_address"`
