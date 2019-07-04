ee4116cf3fb42a12dd736145f13d39b721ef7397
zengxianglin <429801517@qq.com>
2019-07-05 02:26:14 +0800

    增加了币节点的配置类以及

--- Models/MasterNodeModel/CoinNodeConcoct.go
+++ Models/MasterNodeModel/CoinNodeConcoct.go
@@ -1,5 +1,10 @@
 package MasterNodeModel
 
+type Command struct {
+	Key  string
+	Args []string
+}
+
 type CoinNodeConcoct struct {
 	// 币种在中心数据库内的编号
 	CoinIndex uint
@@ -8,8 +13,7 @@ type CoinNodeConcoct struct {
 	// 节点安装文件URL
 	InstallFileURL string
 	// 文件处理命令，节点端不需要关心文件内部是什么样，只需要把文件下载之后，将文件按照以下的命令进行替换内容，需要替换的内容在参数内
-	FileHandleCommand string
-	FileHandleParams  map[string]string
+	FileHandleCommand Command
 	// 节点端不需要关心具体获取状态的方式是什么，只需要按照中心传过来的命令运行，将运行结果回传到中心即可
-	SituationCommand map[string]string
+	SituationCommand map[string]Command
 }
