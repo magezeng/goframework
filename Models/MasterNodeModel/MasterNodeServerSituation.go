d9087e0a4a8d358ee8a8546eba69a96e633150c3
zengxianglin <429801517@qq.com>
2019-08-14 10:22:38 +0800

    MasterNodeService 心跳数据增加版本号

--- Models/MasterNodeModel/MasterNodeServerSituation.go
+++ Models/MasterNodeModel/MasterNodeServerSituation.go
@@ -4,6 +4,7 @@ package MasterNodeModel
 type MasterNodeServerSituation struct {
 	// 没有时不进行设置
 	IP        string  `json:"ip"`
+	Version   string  `json:"version"`
 	CPUUsage  float32 `json:"cpu_usage"`
 	MemUsage  float32 `json:"mem_usage"`
 	DiskUsage float32 `json:"disk_usage"`
